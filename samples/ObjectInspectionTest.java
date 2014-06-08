06b26e3e3b2e7878f9272c11e97c30c2bc76708e
Alfredo Mungo <chimeranet89@gmail.com>
2014-06-08 20:24:42 +0200

    Updated test

--- samples/ObjectInspectionTest.java
+++ samples/ObjectInspectionTest.java
@@ -1,3 +1,4 @@
+import wisedevil.test.MethodInspector;
 import wisedevil.test.PropertyInspector;
 import wisedevil.test.annotation.*;
 
@@ -8,6 +9,13 @@ import static wisedevil.test.Assert.*;
 public class ObjectInspectionTest {
 	public class TestClass {
 		private int myProperty = 5;
+		private void myVoidMethod() {
+			System.out.println("myVoidMethod() called");
+		}
+
+		private int mySumMethod(int a, int b) {
+			return a + b;
+		}
 	}
 	
 	@Test
@@ -19,5 +27,21 @@ public class ObjectInspectionTest {
 		oi.set(6);
 		
 		assert (int)oi.get() == 6;
+	}
+
+	@Test
+	public void method_invoke() {
+		TestClass tc = new TestClass();
+		MethodInspector mi = new MethodInspector(tc, "myVoidMethod");
+		MethodInspector mi_sum = new MethodInspector(tc, "mySumMethod", int.class, int.class);
+
+		try {
+			mi.invoke();
+
+			assert (int)mi_sum.invoke(2, 3) == 5;
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail();
+		}
 	}
 }
